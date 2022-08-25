4022bc3f3a802d24f2ea774460b814c5ebf62d41
Pratham2301 <prathamrajbhoj2003@gmail.com>
2022-08-25 15:03:54 +0530

    minor changes

--- sih22/urls.py
+++ sih22/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 from rest_framework import permissions
@@ -19,6 +22,10 @@ schema_view = get_schema_view(
 	permission_classes=(permissions.AllowAny,),
 )
 
+from django.views.static import serve
+# from django.conf.urls import url
+from django.urls import re_path as url
+
 urlpatterns = [
 	path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 	path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
@@ -30,6 +37,9 @@ urlpatterns = [
     path('city_api',views.city_api),
     path('city_increment',views.city_increment),
     
-    path('update_score',views.update_Score)
+    path('update_score',views.update_Score),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
